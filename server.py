144020710dce70416b8dcce25493c24322173cca
ValeriiaSedova <valeriiasedova@gmail.com>
2024-04-02 22:53:39 +0200

    Added file with server method

--- server.py
+++ server.py
@@ -1,13 +1,21 @@
 import socket
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind(('0.0.0.0', 12345))  # Listen on all network interfaces, port 12345
-server_socket.listen()
-print("Waiting for connection...")
-connection, address = server_socket.accept()
-print(f"Connected to {address}")
-while True:   
-    message = connection.recv(1024).decode('utf-8')
-    if not message:
-        break  # End the loop if message is empty
-    print(f"Received message: {message}")
-connection.close()
+
+port_Valeriia = 123
+port_Sergei   = 124
+
+def server(port: int):
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(('0.0.0.0', port))  # Listen on all network interfaces, port 12345
+    server_socket.listen()
+    print("Waiting for connection...")
+    connection, address = server_socket.accept()
+    print(f"Connected to {address}")
+    while True:   
+        message = connection.recv(1024).decode('utf-8')
+        if not message:
+            break  # End the loop if message is empty
+        print(f"Received message: {message}")
+    connection.close()
+
+if __name__ == "__main__":
+    server(port_Valeriia)
