1a5902e7815ff0fb1846e7c088ad908c69b24888
dobrik4ever <dobrik4ever@gmail.com>
2024-04-02 22:45:36 +0200

    modified client to be a method

--- client.py
+++ client.py
@@ -1,9 +1,16 @@
 import socket
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect(('192.168.178.26', 123))  # Replace with your husband's laptop IP address
-while True:
-    message = input("Enter your message: ")    
-    if message == "exit":
-        break  # Type 'exit' to close the connection    
-    client_socket.send(message.encode('utf-8'))
-client_socket.close()
+address_Valeriia = ('192.168.178.26', 123)
+address_Sergei = ('192.168.178.25', 124)
+
+def client(ip:str, port:int):
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((ip, port))  # Replace with your husband's laptop IP address
+    while True:
+        message = input("Enter your message: ")    
+        if message == "exit":
+            break  # Type 'exit' to close the connection    
+        client_socket.send(message.encode('utf-8'))
+    client_socket.close()
+
+if __name__ == '__main__':
+    client(*address_Valeriia)
